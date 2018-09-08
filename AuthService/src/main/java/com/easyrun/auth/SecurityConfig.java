b1aa30f7fa2d05cde900c291710415a26c60c824
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-09-08 18:34:35 -0500

    add acuator

--- AuthService/src/main/java/com/easyrun/auth/SecurityConfig.java
+++ AuthService/src/main/java/com/easyrun/auth/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.easyrun.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.easyrun.auth.security.UsernamePasswordAuthentitationProvider;
 import com.easyrun.auth.security.UsernamePasswordFilter;
@@ -44,9 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+		/*http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
 				.antMatchers("/encode/**").permitAll().antMatchers("/token/**").hasAuthority("user.token");
-		http.csrf().disable();
+		http.csrf().disable();*/
+		http
+	    .authorizeRequests()
+	        // 1
+	        .requestMatchers(EndpointRequest.to("status", "info")).permitAll();
 	}
 
 	@Bean
