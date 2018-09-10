ee63514cf83fe21e5bf20ba10985c421a153d747
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-09-10 11:04:54 -0500

    testing

--- Configuration/src/main/java/com/easyrun/configuration/ConfigApplication.java
+++ Configuration/src/main/java/com/easyrun/configuration/ConfigApplication.java
@@ -1,9 +1,12 @@
 package com.easyrun.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,6 +15,14 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer
 public class ConfigApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(ConfigApplication.class, args);	
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
 	}
 }
