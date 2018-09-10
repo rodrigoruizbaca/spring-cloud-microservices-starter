99e68582002c6538b0f3c81ab270933e31c29042
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-09-10 13:09:58 -0500

    bus tests

--- startrabbit.sh
+++ startrabbit.sh
@@ -1,1 +1,2 @@
-docker run -d --hostname my-rabbit -p 5672:5672 rabbitmq:3
+#docker run -d --hostname my-rabbit -p 5672:5672 rabbitmq:3
+docker run -d --hostname my-rabbit -p 5672:5672 -p 15672:15672 rabbitmq:3-management
