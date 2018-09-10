c7eb1a0e3a170972ceaedefa1ddd823c4180005b
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-09-10 11:30:55 -0500

    tests

--- Discovery/src/main/java/com/easyrun/discovery/controller/DiscoveryController.java
+++ Discovery/src/main/java/com/easyrun/discovery/controller/DiscoveryController.java
@@ -1,11 +1,15 @@
 package com.easyrun.discovery.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Maps;
 
 @RestController()
 @CrossOrigin
@@ -16,7 +20,9 @@ public class DiscoveryController {
 	private String test;
 	
 	@GetMapping("test")	
-	public String test() {
-		return test;
+	public Map<String, Object> test() {
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("test", test);
+		return map;
 	}
 }
